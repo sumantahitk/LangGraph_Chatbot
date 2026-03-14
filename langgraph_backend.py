ee631372999b36020d7ee1f209173b83ffba85cc
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-14 11:41:56 +0530

    add streaming functionality

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,9 +1,10 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage ,HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -31,3 +32,8 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
+
+
+
---
+++ streamlit_frontend_streaming.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+CONFIG= {'configurable':{'thread_id':'thread-1'}}
+# st.session_state ->dict
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+
+# message_history=[]
+
+# {'role':'user','content':'Hi'}
+# {'role':'assistent','content':''}
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input=st.chat_input('Type here ...')
+
+if user_input:
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # response=chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=CONFIG)
+
+    # ai_message=response['messages'][-1].content
+
+    
+    with st.chat_message('assistant'):
+        ai_message=st.write_stream(
+            message_chunk.content for message_chunk, metadata in  chatbot.stream(
+            {'messages':[HumanMessage(content=user_input)]},
+            config= {'configurable':{'thread_id':'thread-1'}},
+             stream_mode='messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
